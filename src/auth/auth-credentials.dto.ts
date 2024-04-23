9bcde9ab06af40eccb62696b9672f975f1083ac7
dhia-eddine <diamandouj@gmail.com>
2024-04-23 20:41:26 +0100

    a

--- src/auth/auth-credentials.dto.ts
+++ src/auth/auth-credentials.dto.ts
@@ -1,19 +1,25 @@
-import { IsNotEmpty, IsString, Matches, MaxLength, MinLength } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class AuthCredentialsDto {
-@IsNotEmpty()
-@IsString()
-email: string;
+  @IsNotEmpty()
+  @IsString()
+  email: string;
 
-@IsNotEmpty()
-@IsString()
-@MinLength(8)
-@MaxLength(50)
-@Matches(
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(8)
+  @MaxLength(50)
+  @Matches(
     /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/,
     {
-    message: 'Password too weak',
+      message: 'Password not much ',
     },
-)
-password: string;
+  )
+  password: string;
 }
